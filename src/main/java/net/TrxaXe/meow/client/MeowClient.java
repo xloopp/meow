ab3a4cb8123e824addf2b2d8aa02ba1405833e0d
xloopp <1991315247@qq.com>
2025-07-22 13:47:13 +0800

    t

--- src/main/java/net/TrxaXe/meow/client/MeowClient.java
+++ src/main/java/net/TrxaXe/meow/client/MeowClient.java
@@ -22,7 +22,7 @@ public class MeowClient implements ClientModInitializer {
         ClientReceiveMessageEvents.MODIFY_GAME.register((message, overlay) -> {
             ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
             String s = message.getString();
-            if ((s.indexOf('喵') != -1 && s.indexOf('\u200C') != -1) && s.indexOf('呜') != -1 && config.DecodeMeow) {
+            if ((s.indexOf('喵') != -1 || s.indexOf(' ') != -1) && s.indexOf('\u200C') != -1 && s.indexOf('呜') != -1 && config.DecodeMeow) {
                 Gson gson = new Gson();
                 String json = gson.toJson(TextCodecs.CODEC.encodeStart(JsonOps.INSTANCE, message).getOrThrow());
                 Text DecodedMessage = TextCodecs.CODEC
